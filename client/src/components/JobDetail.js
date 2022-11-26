7533b9bbee8adf8205016f279aa8aa871090a264
Dico Diaz <dicodiaz@gmail.com>
2022-11-26 18:22:09 -0500

    Query Variables

--- client/src/components/JobDetail.js
+++ client/src/components/JobDetail.js
@@ -1,26 +1,30 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
-import { jobs } from '../fake-data';
+import { getJob } from '../graphql/queries';
 
-function JobDetail() {
+const JobDetail = () => {
+  const [job, setJob] = useState(null);
   const { jobId } = useParams();
 
-  const job = jobs.find((job) => job.id === jobId);
+  useEffect(() => {
+    getJob(jobId).then(setJob);
+  }, [jobId]);
+
+  if (!job) return <p>Loading...</p>;
+
+  const { title, description, company } = job;
+  const { id: companyId, name: companyName } = company;
+
   return (
     <div>
-      <h1 className="title">
-        {job.title}
-      </h1>
+      <h1 className="title">{title}</h1>
       <h2 className="subtitle">
-        <Link to={`/companies/${job.company.id}`}>
-          {job.company.name}
-        </Link>
+        <Link to={`/companies/${companyId}`}>{companyName}</Link>
       </h2>
-      <div className="box">
-        {job.description}
-      </div>
+      <div className="box">{description}</div>
     </div>
   );
-}
+};
 
 export default JobDetail;
--- client/src/graphql/queries.js
+++ client/src/graphql/queries.js
@@ -2,6 +2,25 @@ import { gql, request } from 'graphql-request';
 
 const GRAPHQL_URL = 'http://localhost:9000/graphql';
 
+export const getJob = async (jobId) => {
+  const query = gql`
+    query JobQuery($jobId: ID!) {
+      job(id: $jobId) {
+        id
+        title
+        description
+        company {
+          id
+          name
+        }
+      }
+    }
+  `;
+  const variables = { jobId };
+  const { job } = await request(GRAPHQL_URL, query, variables);
+  return job;
+};
+
 export const getJobs = async () => {
   const query = gql`
     query {
